6f5a7025e282baa079d435e3e49eec202b046e3e
sgazzoni <sgazzoni.sg@gmail.com>
2015-12-15 13:56:35 +0100

    creazione pagine html

--- app/Article.php
+++ app/Article.php
@@ -7,13 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     	//serve in fase di aggiornamento, a dire al programma che i dati devono essere accettati. (sono sicuri)
-	protected $fillable = ['title','body','codice_prodotto','disponibile,'prezzo'];
-	protected $fillable = ['nome','cognome','email','telefono'];			
-	
-	$articles = Article::create([
-			'title' => $input['title'],
-			'body' => $input['body'],
-			'codice_prodotto' => $input['codice_prodotto'],
-			'disponibile' => $input['disponibile'],
-			'prezzo' => $input['prezzo'],
+	protected $fillable = ['title','body','codice_prodotto','disponibile','prezzo'];
 }
